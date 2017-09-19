457f390d888e7e91aab61a6edfe69f5a379f1a9b
Larissa <larissinha-ll@hotmail.com>
2017-09-19 14:54:56 -0300

    foi criado o metodo alterar na classe clientedao

--- VendaPecas/src/DAO/ClienteDAO.java
+++ VendaPecas/src/DAO/ClienteDAO.java
@@ -48,4 +48,26 @@ public class ClienteDAO {
         pst.close();
         
     }
+    
+    public void alterar(Cliente cliente) throws SQLException{
+    
+        sql = "update cliente set nome_cliente=?, data_nasc=?, endereco=?, numero=?, bairro=?, cidade=?, estado=?, email=?, cpf=?, rg=?, telefone_comercial=?, telefone=?, celular=? where id_cliente=?";
+        pst = Conexao.getInstance().prepareStatement(sql);
+        pst.setString(1, cliente.getNomeCliente());
+        pst.setString(2, cliente.getDataNascimento());
+        pst.setString(3, cliente.getEndereco());
+        pst.setInt(4, cliente.getNumero());
+        pst.setString(5, cliente.getBairro());
+        pst.setString(6, cliente.getCidade());
+        pst.setString(7, cliente.getEstado());
+        pst.setString(8, cliente.getEmail());
+        pst.setString(9, cliente.getCpf());
+        pst.setString(10, cliente.getRg());
+        pst.setInt(11, cliente.getTelefoneComercial());
+        pst.setInt(12, cliente.getTelefone());
+        pst.setInt(13, cliente.getCelular());
+        pst.execute();
+        pst.close();
+    
+    }
 }
